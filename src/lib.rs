ec5b8dc4ec71d55aecfc40f7ce14d1401bc5a842
Nick Dowsett <nickdowsett42@gmail.com>
2026-04-21 08:10:02 +0800

    Add allocator as trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,133 @@
 #![no_std]
 
-use core::{ops::DerefMut, task::Poll};
-
+use core::{
+    any::{Any, TypeId},
+    marker::PhantomData,
+    ops::DerefMut,
+    pin::{pin, Pin},
+    task::Poll,
+};
 use futures::StreamExt;
+use pin_project::pin_project;
 use smallbox::{smallbox, SmallBox};
 
 type NoDepsEffect<T> = dyn futures::Stream<Item = T>;
 type SmallboxSize = smallbox::space::S8;
 
-struct Executor<T, const N: usize> {
+struct Executor<T, const N: usize, A: allocator_api2::alloc::Allocator> {
     next_task_id: u64,
-    task_list: heapless::Vec<SmallBox<NoDepsEffect<T>, SmallboxSize>, N>,
+    last_polled: usize,
+    task_list: heapless::Vec<ExecutorItem<T, A>, N>,
 }
 
-impl<T, const N: usize> Executor<T, N> {
+struct Enumerated<T>(usize, T);
+
+struct ExecutorItem<T, A: allocator_api2::alloc::Allocator> {
+    stream: allocator_api2::boxed::Box<NoDepsEffect<Enumerated<T>>, A>,
+    task_id: u64,
+    task_type_name: &'static str,
+    task_type_id: TypeId,
+}
+impl<T, A: allocator_api2::alloc::Allocator> core::fmt::Debug for ExecutorItem<T, A> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        todo!()
+    }
+}
+
+enum EffectOutput<T> {
+    Finished {
+        task_id: u64,
+        task_type_name: &'static str,
+        task_type_id: TypeId,
+    },
+    Continuing {
+        task_id: u64,
+        task_type_name: &'static str,
+        task_type_id: TypeId,
+        output_n: usize,
+        output: T,
+    },
+}
+
+impl<T, const N: usize, A: allocator_api2::alloc::Allocator> Executor<T, N, A> {
     fn new() -> Self {
         Self {
             next_task_id: 0,
             task_list: heapless::Vec::new(),
+            last_polled: 0,
         }
     }
-    fn push(&mut self, t: impl futures::Stream<Item = T> + 'static) {
-        self.task_list[0] = smallbox!(t)
+    fn push(&mut self, stream: impl futures::Stream<Item = T> + Any + 'static, alloc: A) {
+        let task_id = self.next_task_id;
+        self.next_task_id += 1;
+        let task_type_name = core::any::type_name_of_val(&stream);
+        let task_type_id = stream.type_id();
+        self.task_list
+            .push(ExecutorItem {
+                stream: allocator_api2::unsize_box!(allocator_api2::boxed::Box::new_in(
+                    stream.enumerate().map(|(idx, item)| Enumerated(idx, item)),
+                    alloc,
+                )),
+                task_id,
+                task_type_name,
+                task_type_id,
+            })
+            .unwrap()
     }
-    fn get_next(&mut self) -> GetNext<T> {
+    fn get_next(&mut self) -> GetNext<T, N> {
         GetNext {
-            items: self.task_list.as_mut_slice(),
+            items: &mut self.task_list,
         }
     }
 }
 
-struct GetNext<'a, T> {
-    items: &'a mut [SmallBox<NoDepsEffect<T>, SmallboxSize>],
+#[pin_project]
+struct GetNext<'a, T, const N: usize> {
+    #[pin]
+    items: &'a mut heapless::Vec<ExecutorItem<T>, N>,
 }
 
-impl<'a, T> futures::Future for GetNext<'a, T> {
-    type Output = Option<T>;
+impl<'a, T, const N: usize> futures::Future for GetNext<'a, T, N> {
+    type Output = Option<EffectOutput<T>>;
     fn poll(
         mut self: core::pin::Pin<&mut Self>,
         cx: &mut core::task::Context<'_>,
     ) -> Poll<Self::Output> {
-        for item in self.items.iter_mut() {
-            let item = unsafe { core::pin::Pin::new_unchecked(item.deref_mut()) };
-            match futures::stream::Stream::poll_next(item, cx) {
-                Poll::Ready(None) => return Poll::Ready(None),
-                Poll::Ready(Some(t)) => return Poll::Ready(Some(t)),
+        let len = self.items.len();
+        for idx in 0..len {
+            // NOPANIC: len checked above
+            let stream =
+                unsafe { core::pin::Pin::new_unchecked(self.items[idx].stream.deref_mut()) };
+            match futures::stream::Stream::poll_next(stream, cx) {
+                Poll::Ready(Option::None) => {
+                    // NOPANIC: len checked above
+                    let task_id = self.items[idx].task_id;
+                    let task_type_name = self.items[idx].task_type_name;
+                    let task_type_id = self.items[idx].task_type_id;
+                    self.items.remove(idx);
+                    return Poll::Ready(Some(EffectOutput::Finished {
+                        task_id,
+                        task_type_name,
+                        task_type_id,
+                    }));
+                }
+                Poll::Ready(Option::Some(Enumerated(output_n, output))) => {
+                    // NOPANIC: len checked above
+                    let task_id = self.items[idx].task_id;
+                    let task_type_name = self.items[idx].task_type_name;
+                    let task_type_id = self.items[idx].task_type_id;
+                    return Poll::Ready(Some(EffectOutput::Continuing {
+                        task_id,
+                        task_type_name,
+                        task_type_id,
+                        output_n,
+                        output,
+                    }));
+                }
                 Poll::Pending => (),
             }
         }
-        Poll::Pending
+        Poll::Ready(None)
     }
 }
 
